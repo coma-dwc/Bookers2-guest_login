405c185e00315ca8ffedc489d803e728a6058395
coma-dwc <125862959+tamagomapon@users.noreply.github.com>
2023-04-10 07:43:33 +0000

    [finish]ゲストログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,16 @@ class User < ApplicationRecord
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
-  
-  
+
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+
+  def self.guest
+    find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user| #データの検索と作成を自動的に判断して処理を行うメソッド
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
   end
 end
